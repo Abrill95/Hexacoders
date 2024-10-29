786fcd305f5ace20afa9b43ccac50b7bafc9a953
DESKTOP-JEED34M\ornella <44125811@fi.unju.edu.ar>
2024-10-28 21:45:10 -0300

    Pagina Acerca de.html

--- teamHEXACODERS/core/views.py
+++ teamHEXACODERS/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import HttpResponse, render
 
 def home(request):
     return render(request, "core/index.html")
+
+def acercade(request):
+    return render(request, "core/acercade.html")
