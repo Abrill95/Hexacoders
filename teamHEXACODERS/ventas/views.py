a8192005156319830f486b99759e07c136ae5e2b
Abril Lopez <showna9555@gmail.com>
2024-11-19 20:36:03 -0300

    agregando las siguientes operaciones a los modelos cliente y proveedor: Listado y búsqueda,  Agregar un nuevo cliente-proveedor, Modificar los datos de un cliente-proveedor y eliminar cliente-proveedor

--- teamHEXACODERS/ventas/views.py
+++ teamHEXACODERS/ventas/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Producto, Cliente, Proveedor,Venta
-from django.core.paginator import Paginator
+from django.db.models import Q
+
+from django.views.generic.edit import CreateView
 
 def productos(request):
     productos = Producto.objects.all()
@@ -13,6 +16,85 @@ def productos(request):
     # Obtiene los productos para esa página
     productos_page = paginator.get_page(page_number)  #
     return render(request, "productos.html", {'productos': productos_page})
+
+# Modificando para usar ListView cliente
+class ClienteListView(ListView):
+    model = Cliente
+    template_name = 'lista_clientes.html'
+    context_object_name = 'clientes'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q','')
+        if query:
+            return Cliente.objects.filter(
+                Q(nombre__icontains=query) | 
+                Q(apellido__icontains=query) |
+                Q(email__icontains=query) |
+                Q(celular__icontains=query)
+            )
+        return Cliente.objects.all()
+
+# Modificando para usar ListView proveedor
+class ProveedorListView(ListView):
+    model = Proveedor
+    template_name = 'lista_proveedores.html'
+    context_object_name = 'proveedores'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q','')
+        if query:
+            return Proveedor.objects.filter(
+                Q(nombre__icontains=query) | Q(email__icontains=query)
+            )
+        return Proveedor.objects.all()
+    
+    
+from django.views.generic.edit import CreateView 
+# Usando CreateView pra cliente
+class ClienteCreateView(CreateView):
+    model = Cliente
+    fields = ['nombre', 'apellido', 'email', 'celular', 'foto']  
+    template_name = 'cliente_form.html'
+    success_url = reverse_lazy('ventas:clientes')    
+
+from django.views.generic.edit import UpdateView
+# Usando  UpdateView para cliente
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    fields = ['nombre', 'apellido', 'email', 'celular', 'foto'] 
+    template_name = 'cliente_form.html'
+    success_url = reverse_lazy('ventas:clientes')
+
+# Usando DeleteView para cliente
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    template_name = 'cliente_confirm_delete.html'
+    success_url = reverse_lazy('ventas:clientes')
+
+
+from django.views.generic.edit import CreateView 
+# Usando Create Vieew para Proveedor
+class ProveedorCreateView(CreateView):
+    model = Proveedor
+    fields = ['nombre', 'email']
+    template_name = 'proveedor_form.html'
+    success_url = reverse_lazy('ventas:proveedores')
+
+from django.views.generic.edit import UpdateView
+# Usando  UpdateView para Proveedor
+class ProveedorUpdateView(UpdateView):
+    model = Proveedor
+    fields = ['nombre', 'email'] 
+    template_name = 'proveedor_form.html'
+    success_url = reverse_lazy('ventas:proveedores')
+
+# Usando DeleteView para Proveedor
+class ProveedorDeleteView(DeleteView):
+    model = Proveedor
+    template_name = 'proveedor_confirm_delete.html'
+    success_url = reverse_lazy('ventas:proveedores')
+
+
 
 def listar_clientes(request):
     clientes = Cliente.objects.all()
